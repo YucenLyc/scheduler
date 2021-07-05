b4e35c7ee5e749f930455b20c57e8ec568041e92
YucenLyc <yucen.u@gmail.com>
2021-07-05 16:06:31 -0700

    add Buttons

--- stories/index.js
+++ stories/index.js
@@ -12,13 +12,17 @@ storiesOf("Button", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
   })
   .add("Base", () => <Button>Base</Button>)
+
   .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
+
+  .add("Danger", () => <Button danger>Danger</Button>)
+
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
+
   .add("Disabled", () => (
-    <Button disabled onClick={action("button-clicked")}>
+    <Button disabled>
       Disabled
     </Button>
   ));
