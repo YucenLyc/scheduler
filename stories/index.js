a6147976e3cc844084752c69cc218c502e3900e3
YucenLyc <yucen.u@gmail.com>
2021-07-06 18:11:37 -0700

    Add Empty

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 //Button
 storiesOf("Button", module)
@@ -155,3 +156,5 @@ storiesOf("InterviewerListItem", module)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
 
     .add("Header", () => <Header time="12pm" />)
+
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
