6f297f5bc39aed6aa27910259b99d344ba79c42d
YucenLyc <yucen.u@gmail.com>
2021-07-06 19:05:17 -0700

    Add Confirm component/Refactor

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,8 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+
 
 //Button
 storiesOf("Button", module)
@@ -167,4 +169,13 @@ storiesOf("InterviewerListItem", module)
     onEdit={action("onEdit")}
     onDelete={action("onDelete")}
     />
+    ))
+
+    .add ("Confirm", () => (
+    <Confirm 
+    message="Delete the appointment?"
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+    />
+
     ))
